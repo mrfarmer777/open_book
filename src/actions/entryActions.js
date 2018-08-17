26b174fb8805b10fbd6d8dca49120cc48ecaf85c
mrfarmer7771 <mfarmer@d120.org>
2018-08-17 05:41:51 +0000

    configures proxy for api requests and adds fetch action

--- src/actions/entryActions.js
+++ src/actions/entryActions.js
@@ -3,7 +3,9 @@
 export function fetchEntries() {
   return (dispatch) => {
     dispatch({ type: 'BEGIN_GET_ENTRIES_REQUEST' });
-    return fetch('/api/entries')
+    return fetch(`https://flatiron-2-mrfarmer7771.c9users.io/entries`, {
+    accept: 'application/json',
+  })
       .then(response => response.json())
       .then(entries => dispatch({ type: 'ADD_ENTRIES', payload: entries }));
   };
