0c0f6a05c6a3c107578fee1407861121bd4aa653
Matt Farmer <mfarmer@d120.org>
2018-08-18 22:44:08 +0000

    creating book-has-many-entries relationship and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_18_214347) do
+ActiveRecord::Schema.define(version: 2018_08_18_224110) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_08_18_214347) do
     t.integer "pages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "book_id"
   end
 
 end
