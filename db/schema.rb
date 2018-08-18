7e30c78cac1b3b9a3e5a5ed0915daeb71e25bbf1
Matt Farmer <mfarmer@d120.org>
2018-08-18 22:05:07 +0000

    implemented books API resources and routes on rails backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_034648) do
+ActiveRecord::Schema.define(version: 2018_08_18_214347) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "pages"
+    t.string "genres"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string "title"
