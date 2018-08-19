26d8a5abd70f9a8c51ce0d593b4f6ffa9cc569cd
Matt Farmer <mfarmer@d120.org>
2018-08-19 04:11:54 +0000

    updating statsReducer - creating separate actions for book and entry stats

--- client/src/reducers/statsReducer.js
+++ client/src/reducers/statsReducer.js
@@ -1,15 +1,21 @@
 
 export default function statsReducer(
-    state=[]
+    state={
+        entryStats: {},
+        bookStats: {}
+    }
     ,action){
     
     switch(action.type){
         case "BEGIN_STATS_REQUEST":
             console.log('getting the stats...' + action.payload)
             return state
-        case "ADD_STATS":
-            console.log('updating stats: '+ action.payload)
-            return action.payload
+        case "ADD_ENTRY_STATS":
+            console.log('updating entry stats: '+ action.payload)
+            return {...state, entryStats: action.payload}
+        case "ADD_BOOK_STATS":
+            console.log('updating book stats: '+ action.payload)
+            return {...state, bookStats: action.payload}
         default: 
             return state
     }
