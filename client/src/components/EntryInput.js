9226600073e94850f6162b6b28c4955a9f30480b
Matt Farmer <mfarmer@d120.org>
2018-08-19 05:39:02 +0000

    styling entry input form

--- client/src/components/EntryInput.js
+++ client/src/components/EntryInput.js
@@ -32,23 +32,31 @@ export default class EntryInput extends Component{
     render(){
         
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label for="book_id">Book: </label>
-                <select name="book_id" value={this.state.book_id} onChange={this.handleChange}>
-                    <option value="" default disabled>Select a book</option>
-                    {this.props.books.map(book=>{
-                        
-                        return(
-                            <option key={book.id} value={book.id}>{book.title}</option>
-                        )
-                    })}
-                </select>
-                <label for="time">Minutes: </label>
-                <input type="number" name="time" id="time" step="5" onChange={this.handleChange} value={this.state.time}/>
-                <label for="pages">Pages: </label>
-                <input type="number" name="pages" id="pages" step="1" onChange={this.handleChange} value={this.state.pages}/>
-                <input class="btn btn-default" type="submit" value="New Entry"/>
-            </form>
+            <div class="container">
+                <h3>What are you reading today?</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <div class="input-group">
+                        <label for="book_id">Book: </label>
+                        <select name="book_id" value={this.state.book_id} onChange={this.handleChange}>
+                            <option value="" default disabled>Select a book</option>
+                            {this.props.books.map(book=>{
+                                
+                                return(
+                                    <option key={book.id} value={book.id}>{book.title}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                    <div class="input-group">
+                        <label for="time">Minutes: </label>
+                        <input type="number" name="time" id="time" step="5" onChange={this.handleChange} value={this.state.time}/>
+                    
+                        <label for="pages">Pages: </label>
+                        <input type="number" name="pages" id="pages" step="1" onChange={this.handleChange} value={this.state.pages}/>
+                    </div>
+                    <input class="btn btn-primary" type="submit" value="New Entry"/>
+                </form>
+            </div>
             
             )
     }
