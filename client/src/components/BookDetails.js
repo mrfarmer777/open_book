409b816e418c5112d26c3d076d7a4ae1bfa0ed66
Matt Farmer <mfarmer@d120.org>
2018-08-23 13:44:42 +0000

    adding show book functionality

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 export default class BookDetails extends React.Component{
     
     render(){
         return(
-            <div class="modal" id="bookModal" tabIndex="-1" role="dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5>A Book Title</h5>
-                        <p>A Book Author</p>
-                        <p>Total Pages: 3546</p>
-                        <p>Total Entries: 16</p>
-                    </div>
-                </div>
+            <div class="static-modal" id="bookModal" tabIndex="-1" role="dialog">
+                <Modal.Dialog>
+                    <Modal.Header>
+                        <Modal.Title>Book Details</Modal.Title>
+                     </Modal.Header>
+                </Modal.Dialog>
             
             </div>
             )
