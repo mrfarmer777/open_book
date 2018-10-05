faa2f4cf25a4770ed6a0a3bf0bdcc1b3f97bcc87
Matt Farmer <mfarmer@d120.org>
2018-10-05 01:54:58 +0000

    implementing react-bootstrap responsive navbar

--- client/src/Routers/Router.js
+++ client/src/Routers/Router.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import HomeContainer from '../containers/HomeContainer'
 import BookshelfContainer from '../containers/BookshelfContainer'
 import StatsContainer from '../containers/StatsContainer'
-
 
 
 const Home = () => (
@@ -32,25 +32,29 @@ const Bookshelf = () => (
 const MrRouter = () => (
     <Router>
     <div>
-        <nav class="navbar navbar-default">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" >Dog Ears</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/stats">Stats</Link></li>
-                    <li><Link to="/bookshelf">Bookshelf</Link></li>
-                </ul>
-            </div>
+        <Navbar collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    Dog Ears
+                </Navbar.Brand>
+                <Navbar.Toggle/>
+            </Navbar.Header>
             
-        </nav>
+            <Navbar.Collapse>    
+                <Nav>
+                    <NavItem><Link to="/">Home</Link></NavItem>
+                    <NavItem><Link to="/stats">Stats</Link></NavItem>
+                    <NavItem><Link to="/bookshelf">Bookshelf</Link></NavItem>
+                
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        
+        
+        
+            
+            
+        
         
         
         
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Navbar } from 'react-bootstrap'
 export default class MyNavbar extends Component{
     render(){
         return (
-            <div>I'M the nav-bar, I'M the nav-bar</div>
+            <div>Bleh</div>
         )
     }
 }
