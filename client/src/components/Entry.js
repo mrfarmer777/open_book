2de98709487fbef0e7c5128dffdd326f2309c013
Matt Farmer <mfarmer@d120.org>
2018-10-06 17:57:26 +0000

    refactoring the entries and entry components with react-bootstrap

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Button, Glyphicon} from 'react-bootstrap'
 
 
 export default class Entry extends Component{
@@ -13,7 +14,7 @@ export default class Entry extends Component{
     render(){
         return(
             <li class="list-group-item">
-                <button class="btn btn-danger glyphicon glyphicon-remove pull-right" id={this.props.entry.id} onClick={this.handleClick}></button>
+                <Button bsStyle="danger pull-right" id={this.props.entry.id} onClick={this.handleClick}><Glyphicon glyph="remove"/></Button>
                 <p>Read {this.props.entry.pages} of {this.props.entry.book.title} in {this.props.entry.time} minutes.</p>
             </li>
         )
