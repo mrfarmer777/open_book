e32d60d7c20095f3fd144d8076a949fea490e99b
Matt Farmer <mfarmer@d120.org>
2018-10-06 19:10:47 +0000

    created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def new
+    end
+    
+    #creates a new session if the user password is authenticated
+    def create
+        @user=User.find_by(email: params[:email])
+        return head(:forbidden) unless @user.authenticate(params[:password])
+    end
+    
+    def destroy 
+        session.delete :user_id
+        redirect_to '/'
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    #skipping for now, double check this later...
+    skip_before_action :verify_authenticity_token
+    
+    def index
+        @users=User.all
+        render json: @users
+    end
+    
+    def create
+        @user=User.new(name: user_params[:name], email: user_params[:email], type: user_params[:type], age: user_params[:age])
+        @user.save
+        render json: User.all
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :type, :age, :password)
+    end
 end
