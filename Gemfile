8daccae796865f6ef6eeab39462cf20316a802d4
Matt Farmer <mfarmer@d120.org>
2018-10-16 03:51:44 +0000

    implementing sessions authentication with jwt

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+#web tokens for authentication
+gem 'jwt'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,16 @@ class UsersController < ApplicationController
         render json: @users
     end
     
+    #creating a new user for the first time from a user_params hash
     def create
-        @user=User.new(name: user_params[:name], email: user_params[:email], type: user_params[:type], age: user_params[:age])
-        @user.save
-        session[:user_id]=@user.id
-        render json: User.all
+        @user=User.new(name: user_params[:name], email: user_params[:email], type: user_params[:type], age: user_params[:age], password: user_params[:password])
+        print @user.valid?
+        if @user.save
+            session[:user_id]=@user.id
+            render json: User.find(@user.id)
+        else
+            render json: User.all
+        end
     end
     
     def show
