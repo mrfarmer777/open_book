be60a20d0fd686a364b2272fdb38ee99ba484213
Matt Farmer <mfarmer@d120.org>
2019-03-21 03:43:05 +0000

    handling entries whose books have been deleted showing on the home page

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -15,7 +15,7 @@ export default class Entry extends Component{
         return(
             <li className="list-group-item">
                 <Button variant="danger" className="pull-right" id={this.props.entry.id} onClick={this.handleClick}> X </Button>
-                <p>Read {this.props.entry.pages} pages of {this.props.entry.book.title} in {this.props.entry.time} minutes.</p>
+                <p>Read {this.props.entry.pages} pages of {(!this.props.entry.book) ? "an unknown work" : this.props.entry.book.title} in {this.props.entry.time} minutes.</p>
             </li>
         )
     }
